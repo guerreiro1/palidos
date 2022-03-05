855ca7015fe9bf020d1356e0942d556ea64a3627
guerreiro1 <andreguerreiro1@gmail.com>
2022-03-05 01:01:48 +0000

    deploy to github pages

--- gh-resources/deploy.ps1
+++ gh-resources/deploy.ps1
@@ -1,4 +1,7 @@
 cd ..;
 ng build;
-Copy-Item .\gh-resources\CNAME .\docs\CNAME
-Copy-Item .\docs\index.html .\docs\404.html
+Copy-Item .\gh-resources\CNAME .\docs\CNAME;
+Copy-Item .\docs\index.html .\docs\404.html;
+git add .;
+git commit -m "deploy to github pages";
+git push;
